16dd52e4296271d3b3b8a9141e47efe8030c15bd
choumanhussein <chouman.hussein7@gmail.com>
2022-03-30 18:05:13 +0000

    Task&&Forms

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,32 +1,64 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
-  def _form
+
+  def show
   end
 
   def edit
   end
-
+  
   def index
-    @tasks = Task.all.order(title: :desc)
+    @tasks = Task.all.order(duedate: :desc)
   end
 
   def new
     if params[:back]
-     @task = Task.new(task_params)
-   else
-     @task = Task.new
-   end
+      @task = Task.new(task_params)
+    else
+      @task = Task.new
+    end
   end
 
-  def show
+
+
+  def create
+    @task = Task.new(task_params)
+    if params[:back]
+      render :new
+    else
+      if @task.save
+        flash[:success] = 'Task created'
+        redirect_to tasks_path
+      else
+        flash.now[:danger] = 'Task cannot be created'
+        render :new
+      end
+    end
+  end
+
+  def update
+      if @task.update(task_params)
+        flash[:success] = 'Task updated'
+        redirect_to tasks_path
+      else
+        flash.now[:danger] = 'Task not updated'
+        render :edit
+      end
+    end
+
+  def destroy
+    @task.destroy
+    flash[:success] = 'Task deleted'
+    redirect_to tasks_path
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :content, :duedate, :priority, :status, :id)
   end
 end
-private
-
- def set_task
-   @task = Task.find(params[:id])
- end
-
- def task_params
-     params.require(:task).permit(:title, :content)
-   end
