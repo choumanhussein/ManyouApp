33bc8558bb0d241d9373145752709d1fa115ac92
choumanhussein <chouman.hussein7@gmail.com>
2022-03-30 16:54:39 +0000

    C&&V IMP

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
   def _form
   end
 
@@ -6,11 +7,26 @@ class TasksController < ApplicationController
   end
 
   def index
+    @tasks = Task.all.order(title: :desc)
   end
 
   def new
+    if params[:back]
+     @task = Task.new(task_params)
+   else
+     @task = Task.new
+   end
   end
 
   def show
   end
 end
+private
+
+ def set_task
+   @task = Task.find(params[:id])
+ end
+
+ def task_params
+     params.require(:task).permit(:title, :content)
+   end
