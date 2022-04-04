3f4634a19b7287442f2e76e632360dcf9edfd354
choumanhussein <chouman.hussein7@gmail.com>
2022-04-04 10:42:02 +0000

    spec++++

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,20 +1,9 @@
 FactoryBot.define do
   factory :task do
-     step2
     title { 'title test' }
     content { 'content test 1' }
   end
-  factory :second_task, class: Task do
-    title { 'title test 2' }
-    content { 'content test 2' }
-  end
-  factory :third_task, class: Task do
-    title { 'title test 3' }
-    content { 'content test 3' }
-    title { 'test1' }
-    content { 'content test 1' }
-    duedate { '2022-03-30 11:00:00' }
-  end
+
   factory :second_task, class: Task do
     title { 'test2' }
     content { 'content test 2' }
@@ -24,6 +13,5 @@ FactoryBot.define do
     title { 'sample3' }
     content { 'sample 3' }
     duedate{ '2022-03-31 19:00:00' }
-   master
   end
 end
