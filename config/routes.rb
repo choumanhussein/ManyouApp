3a3b50a822bd9f9034da4e28c69ab364f7140f18
choumanhussein <chouman.hussein7@gmail.com>
2022-04-07 20:44:26 +0000

    RootConf

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  namespace :admin do
-  resources :users
-  end
+
   get 'sessions/new'
   root 'tasks#index'
   resources :tasks
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users
+  resources :users, only:[:new, :create, :show]
   resources :sessions
+  namespace :admin do
+  resources :users, only:[:index, :edit, :update, :new, :create, :show, :destroy]
+  end
 end
